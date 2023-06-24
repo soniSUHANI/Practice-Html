0fe33cdb6abf5efb85d54ca270f1ae249d43ad9a
soniSUHANI <soni03suhani@gmail.com>
2023-06-24 20:36:45 +0530

    console log and type of variables in js

---
+++ file2.js
@@ -0,0 +1,25 @@
+// console.log("I Like Pizza!");
+// console.log("It's really good!");
+// window.alert("I REALLY LOVE PIZZA!");
+
+// A variable is a container for storing data
+//A variable behaves as if it was the value that ist contains
+
+//Two steps:
+//1. Declaration(var,let,const)
+//2. Assignment (=assignment operator)
+
+let firstName = "Bro"; //strings
+let age = 21; //numbers
+let student = false; //booleans
+
+age=age+1;
+
+console.log("Hello",firstName);
+console.log("You are",age,"Years old");
+console.log("Enrolled",student);
+
+document.geetElementById("p1").innerHTML = "Hello" + firstName;
+document.geetElementById("p2").innerHTML = "you are" + age + "years old";
+document.geetElementById("p1").innerHTML = "Enrolled" + student;
+
