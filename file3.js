aa946fc089f770d603a33d35d13b92df45e6da29
soniSUHANI <soni03suhani@gmail.com>
2023-06-26 11:31:39 +0530

    arithmetic operations in js

---
+++ file3.js
@@ -0,0 +1,33 @@
+/* arithmetic expression is a combination of ...
+   operands( values, variables, etc.)
+   operators(+ - * / %)
+   that can be evaluated to a value
+   ex y = x + 5;
+*/
+
+let students = 35;
+// students = students + 1;
+// students = students - 1;
+// students = students  * 2;
+// students = students  / 5;
+// students = students % 3;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 3;
+
+/*
+   operator precedence
+   1. paranthesis ()
+   2. exponents
+   3. multiplication & division
+   4. addition & subtraction
+*/s
+
+
+
+
+
+
+console.log(students);
