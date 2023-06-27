55c61c628cba564beef703940973c68407f2fb3f
soniSUHANI <soni03suhani@gmail.com>
2023-06-27 23:14:45 +0530

    area of circle in js

---
+++ file6.js
@@ -0,0 +1,13 @@
+//const = avariable that can't be changed
+
+let pi =  3.14159
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle");
+radius = Number(radius);
+
+
+circumference = 2* pi * radius;
+
+console.log("The circumference is: ",circumference);
