eb1752131ec721a0517ce778fc40268c544721e2
soniSUHANI <soni03suhani@gmail.com>
2023-07-01 10:20:35 +0530

    method chaining in js

---
+++ file13.js
@@ -0,0 +1,10 @@
+// metchod chaining = calling one method after another
+//                    in one continuous line of code
+
+let userName = "soni";
+
+
+let letter = userName.charAt(0).toUpperCase().trim();
+//letter = letter.toUpperCase();
+
+console.log(letter);
