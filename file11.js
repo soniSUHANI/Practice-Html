0e6984cf790b056d8a73ff4af608ef834728bad2
soniSUHANI <soni03suhani@gmail.com>
2023-07-01 09:45:40 +0530

    stings uses, properties and methods in javascript

---
+++ file11.js
@@ -0,0 +1,18 @@
+//useful string properties and methods
+
+let userName = "Bro Code";
+let phoneNumber = "123-456-7890";
+
+// console.log(userName.length);
+// console.log(userName.charAt(0));
+// console.log(userName.indexOf("o"));
+// console.log(userName.lastIndexOf("o"));
+
+// userName = userName.toUpperCase();
+// userName = userName.toLowerCase();
+
+phoneNumber = phoneNumber.replaceAll("-", "");
+
+
+console.log(userName);
+console.log(phoneNumber);
