8386cc8472c996813ed0e7adcecd84e2f8f6c8cd
soniSUHANI <soni03suhani@gmail.com>
2023-07-04 22:57:02 +0530

    not operator uses in js

---
+++ file18.js
@@ -0,0 +1,20 @@
+// ! NOT logical operator
+// typically used to reverse a condition's boolean value
+// true -> false false -> true
+
+let temp = 15;
+let sunny = true;
+
+if(!(temp > 0)){
+    console.log("it's cold outside");
+}
+else {
+    console.log("it's warm outside ");
+}
+
+if(sunny){
+    console.log("It's sunny outside");
+}
+else{
+    console.log("it's not sunny outside");
+}
