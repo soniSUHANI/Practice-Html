297d056d9ac4532d40ad1eff06efbaf91b90e5c3
soniSUHANI <soni03suhani@gmail.com>
2023-07-14 12:58:30 +0530

    arrray.reduce() method uses which reduces an array in the way we want

--- file4.js
+++ file4.js
@@ -5,13 +5,13 @@
 //let username = window.prompt("what's your name?");
 // console.log(username);
 
-// DIFFICUL WAY HTML textbox
+// DIFFICULT WAY HTML textbox
 
 let username;
 
 document.getElementById("myButton").onclick = function(){
     username = document.getElementById("myText").value;
     console.log(username);
-    document.getElementById("myLebel").innerHTML = "Hello" + username;
+    document.getElementById("myLebel").innerHTML = "Hello " + username;
 
 }
--- file7.js
+++ file7.js
@@ -2,11 +2,11 @@ let x= 3.14;
 
 a = Math.round(x);
 b = Math.floor(x);
-c = Math.coil(x);
+
 d = Math.pow(x, 2);
 
 
 console.log(a);
 console.log(b);
-console.log(c);
+
 console.log(d);
